1021e6a43a4bff12946e9516caf96ce072843a62
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-06-07 14:32:31 +0530

    Update Assignment1_2.c

--- LB Assignment-1/Assignment1_2.c
+++ LB Assignment-1/Assignment1_2.c
@@ -19,5 +19,3 @@ int main()
 	
 	return 0;
 }
-
-I have completed approximately 70% of the case file query, but I'm encountering an issue with retrieving the customer number from the customer table. Despite my efforts, I haven't been able to resolve this problem. Could you please arrange for DBA support to assist with this?
