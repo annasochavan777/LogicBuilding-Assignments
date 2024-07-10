278a0762abeed8a3143df44cad42977f570e8451
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-10 09:39:52 +0530

    Update Assignment1_1.c

--- LB Assignment-1/Assignment1_1.c
+++ LB Assignment-1/Assignment1_1.c
@@ -1,1 +1,1 @@
-test
+The Chef script has been checked into Stash. Although we don't have a health check dashboard available for the test environment, we have all the necessary steps documented. Therefore, we proceed directly in production.
