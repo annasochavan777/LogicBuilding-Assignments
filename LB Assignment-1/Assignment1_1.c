efdef363c8c03146a405293cb5e39cf132624f1f
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-10 11:43:20 +0530

    Update Assignment1_1.c

--- LB Assignment-1/Assignment1_1.c
+++ LB Assignment-1/Assignment1_1.c
@@ -1,1 +1,8 @@
-The Chef script has been checked into Stash. Although we don't have a health check dashboard available for the test environment, we have all the necessary steps documented. Therefore, we proceed directly in production.
+<trace autoflush="true">
+      <listeners>
+        <add name="dailyLogFileListener" 
+             type="System.Diagnostics.TextWriterTraceListener" 
+             initializeData="log_$(Date).txt" />
+        <remove name="Default" />
+      </listeners>
+    </trace>
