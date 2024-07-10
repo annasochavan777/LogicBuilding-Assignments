69f02e37fcfee74874ee8ce383e80f7df9071caa
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-10 16:53:50 +0530

    Update Assignment1_1.c

--- LB Assignment-1/Assignment1_1.c
+++ LB Assignment-1/Assignment1_1.c
@@ -1,8 +1,40 @@
-<trace autoflush="true">
-      <listeners>
-        <add name="dailyLogFileListener" 
-             type="System.Diagnostics.TextWriterTraceListener" 
-             initializeData="log_$(Date).txt" />
-        <remove name="Default" />
-      </listeners>
-    </trace>
+using System;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SetupDailyLogFile();
+
+        // Example log messages
+        Trace.WriteLine("This is a trace log message.");
+        Trace.WriteLine("Logging another message with the date as the file name.");
+
+        // Flush and close the Trace Listeners at the end of the application
+        Trace.Flush();
+        foreach (TraceListener listener in Trace.Listeners)
+        {
+            listener.Close();
+        }
+    }
+
+    static void SetupDailyLogFile()
+    {
+        // Get the current date
+        string date = DateTime.Now.ToString("yyyy-MM-dd");
+        
+        // Create the log file name with the date
+        string logFileName = $"log_{date}.txt";
+        
+        // Create the TextWriterTraceListener for the log file
+        TextWriterTraceListener listener = new TextWriterTraceListener(logFileName);
+        
+        // Add the listener to the Trace Listeners
+        Trace.Listeners.Add(listener);
+        
+        // Optionally, you can also add the listener to Debug Listeners if needed
+        Debug.Listeners.Add(listener);
+    }
+}
