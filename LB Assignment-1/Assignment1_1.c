c5300ca72e671681060ac05acb3630810da20a7d
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-11 11:19:07 +0530

    Update Assignment1_1.c

--- LB Assignment-1/Assignment1_1.c
+++ LB Assignment-1/Assignment1_1.c
@@ -2,3 +2,5 @@ It seems there is an issue with the case data, as some details, such as product 
 
   
 It appears that the product details are missing from the case, but it's unclear how the case was submitted without them. This seems to be an issue on SJI's end. Could someone investigate this problem?
+
+  It appears there is an issue with the case data, as some details, such as product information, are missing. Without these details, it's impossible to submit the case. Additionally, the MI extractor is failing to extract other cases. I am currently investigating these issues.
