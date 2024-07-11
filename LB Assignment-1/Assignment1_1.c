0def049efac18ab2d791198a9ab34f4c7d18791e
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-11 11:15:07 +0530

    Update Assignment1_1.c

--- LB Assignment-1/Assignment1_1.c
+++ LB Assignment-1/Assignment1_1.c
@@ -1,1 +1,4 @@
 It seems there is an issue with the case data, as some details, such as product information, are missing. Without product details, it's impossible to submit the case. Can someone look into this issue?
+
+  
+It appears that the product details are missing from the case, but it's unclear how the case was submitted without them. This seems to be an issue on SJI's end. Could someone investigate this problem?
