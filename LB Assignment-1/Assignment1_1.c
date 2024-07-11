f04fdad2946b7040c0efff4d1a1c48982193d3d5
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-11 18:17:34 +0530

    Update Assignment1_1.c

--- LB Assignment-1/Assignment1_1.c
+++ LB Assignment-1/Assignment1_1.c
@@ -1,18 +1,1 @@
-Sure, here's the rephrased text:
-
----
-
-Hi Jay,
-
-We have identified the root cause of the MI extractor service failure. The issue is that the data item value below is being received as a string, but it is defined as an integer in the database. This discrepancy is causing the insertion to fail. 
-
-To resolve this, changes are needed on the MSO side, and the SMI team needs to correct the value.
-
-Field in question:
-Customers.1.CreditHistory.ArrearsHighestMonths
-
-Note: This is just one of the fields we have identified so far. We need to review all fields where default values are being set to ensure there are no similar issues.
-
----
-
-Let me know if any further adjustments are needed!
+We have resolved this issue on the MSO side. Now, SJI needs to make the necessary changes on their end to send the correct value.
