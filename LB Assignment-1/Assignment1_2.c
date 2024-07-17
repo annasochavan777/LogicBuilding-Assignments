58f355cddea505e6f964aa66e35f6ffa492a4c3f
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-17 18:46:05 +0530

    Update Assignment1_2.c

--- LB Assignment-1/Assignment1_2.c
+++ LB Assignment-1/Assignment1_2.c
@@ -1,10 +1,13 @@
-
-**Problem Statement:**
-
-If the PCSM API changes do not go live with MSO in production, the new fields introduced will fail at RMI validation.
-
-**Solution:** 
-
-To address this issue, we have added a switch in MSO. When the switch is OFF, it will not consider the new MAE fields, thus preventing the PCSM call from failing.
-
-This change requires a code merge into the release branch and deployment in SIT.
+<?xml version="1.0" encoding="utf-8" ?>
+<nlog xmlns="http://www.nlog-project.org/schemas/NLog.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <targets>
+    <!-- Define a target for file logging -->
+    <target xsi:type="File" name="fileLogger" fileName="${basedir}/logs/log_${shortdate}.log"
+            layout="${longdate}|${level:uppercase=true}|${logger}|${message}" />
+  </targets>
+  
+  <rules>
+    <!-- Define logging rules -->
+    <logger name="*" minlevel="Info" writeTo="fileLogger" />
+  </rules>
+</nlog>
