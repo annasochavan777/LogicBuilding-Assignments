8dfeac56a563314eae8ccf9d89b00f772f1d5ae5
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-17 18:35:18 +0530

    Update Assignment1_2.c

--- LB Assignment-1/Assignment1_2.c
+++ LB Assignment-1/Assignment1_2.c
@@ -1,27 +1,3 @@
-using System;
-using Microsoft.Practices.EnterpriseLibrary.Logging;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        // Load the logging configuration
-        Logger.SetLogWriter(new LogWriterFactory().Create());
-
-        // Log a message
-        Logger.Write("This is an information message.", "General");
-
-        // Log an error message with additional properties
-        LogEntry logEntry = new LogEntry
-        {
-            Message = "This is an error message.",
-            Severity = System.Diagnostics.TraceEventType.Error,
-            Title = "Error",
-            Categories = new List<string> { "General" }
-        };
-
-        Logger.Write(logEntry);
-
-        Console.WriteLine("Logging complete.");
-    }
-}
+Install-Package NLog
+Install-Package NLog.Config
+Install-Package NLog.Schema
