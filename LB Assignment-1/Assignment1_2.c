c831b1bb6a75c06ee6266bcb69627e5471a6fecc
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-17 18:40:57 +0530

    Update Assignment1_2.c

--- LB Assignment-1/Assignment1_2.c
+++ LB Assignment-1/Assignment1_2.c
@@ -1,3 +1,10 @@
-Install-Package NLog
-Install-Package NLog.Config
-Install-Package NLog.Schema
+
+**Problem Statement:**
+
+If the PCSM API changes do not go live with MSO in production, the new fields introduced will fail at RMI validation.
+
+**Solution:** 
+
+To address this issue, we have added a switch in MSO. When the switch is OFF, it will not consider the new MAE fields, thus preventing the PCSM call from failing.
+
+This change requires a code merge into the release branch and deployment in SIT.
