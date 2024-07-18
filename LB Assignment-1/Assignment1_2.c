fcf0e8b4c4080f629ed9c2d698b4e43d87b6fa30
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-18 10:56:47 +0530

    Update Assignment1_2.c

--- LB Assignment-1/Assignment1_2.c
+++ LB Assignment-1/Assignment1_2.c
@@ -1,13 +1,1 @@
-<?xml version="1.0" encoding="utf-8" ?>
-<nlog xmlns="http://www.nlog-project.org/schemas/NLog.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <targets>
-    <!-- Define a target for file logging -->
-    <target xsi:type="File" name="fileLogger" fileName="${basedir}/logs/log_${shortdate}.log"
-            layout="${longdate}|${level:uppercase=true}|${logger}|${message}" />
-  </targets>
-  
-  <rules>
-    <!-- Define logging rules -->
-    <logger name="*" minlevel="Info" writeTo="fileLogger" />
-  </rules>
-</nlog>
+It is not possible to change this in a single place. We need to update all the locations where the log functions are invoked, as the Nlog function names are different.
