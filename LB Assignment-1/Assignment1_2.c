2cd88cea515a1be5fd28bab7708d3a49e62a843f
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-18 14:58:34 +0530

    Update Assignment1_2.c

--- LB Assignment-1/Assignment1_2.c
+++ LB Assignment-1/Assignment1_2.c
@@ -1,15 +1,1 @@
-<nlog>
-  <targets>
-    <target xsi:type="File" name="logfile" 
-            fileName="logs/${date:format=dd-MM-yyyy}/${date:format=HH}.log"
-            archiveEvery="Hour"
-            archiveNumbering="DateAndSequence"
-            archiveDateFormat="yyyyMMddHH"
-            maxArchiveFiles="6"
-            />
-  </targets>
-
-  <rules>
-    <logger name="*" minlevel="Info" writeTo="logfile" />
-  </rules>
-</nlog>
+fileName="logs/${date:format=dd-MM-yyyy}/${date:format=dd-MM-yyyy-HH}-{#}.log"
