2ce115b7cf3dd1321d09482d544d8d20992fde83
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-20 14:07:55 +0530

    Update Assignment1_2.c

--- LB Assignment-1/Assignment1_2.c
+++ LB Assignment-1/Assignment1_2.c
@@ -1,17 +1,8 @@
-Certainly! Here is a rephrased version of the email:
-
----
-
-Hi Sarat,
-
-I'm not sure about the root cause analysis, but should I go ahead and execute the command below to free up some space? This issue is causing delays in our builds.
-
-Best Regards,
-
-Anirasa Chavan  
-Senior Developer | Mortgages | Barclays UK  
-+91 9503203846 | [email](mailto:anirasa.chavan2@barclays.com)
-
----
-
-Does this work for you?
+public void ProcessRequest(string mandatoryParam)
+{
+    if (string.IsNullOrEmpty(mandatoryParam))
+    {
+        throw new ArgumentNullException(nameof(mandatoryParam), "Mandatory parameter is missing.");
+    }
+    // process the request
+}
