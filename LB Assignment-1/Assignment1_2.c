7f96ad8696a5952a67132c0037afa6c4d0466cae
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-22 18:00:54 +0530

    Update Assignment1_2.c

--- LB Assignment-1/Assignment1_2.c
+++ LB Assignment-1/Assignment1_2.c
@@ -1,16 +1,1 @@
-<?xml version="1.0" encoding="utf-8" ?>
-<nlog xmlns="http://www.nlog-project.org/schemas/NLog.xsd"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-
-  <targets>
-    <!-- Define a target for the log files -->
-    <target name="logfile" xsi:type="File"
-            fileName="${basedir}/logs/log_${shortdate}_${hour}.log"
-            layout="${longdate} ${logger} ${message}" />
-  </targets>
-
-  <rules>
-    <!-- Log all messages to the log file -->
-    <logger name="*" minlevel="Trace" writeTo="logfile" />
-  </rules>
-</nlog>
+Subject: Urgent: Performance Issues with Jenkins Build Controller Causing Delays
