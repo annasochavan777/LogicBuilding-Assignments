6388a0cfdc0412b273b659e19aac2ffbb992e1e8
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-22 17:39:47 +0530

    Update Assignment1_2.c

--- LB Assignment-1/Assignment1_2.c
+++ LB Assignment-1/Assignment1_2.c
@@ -1,1 +1,16 @@
-We need to fix this issue and provide details in the response to the caller explaining why the call has failed.
+<?xml version="1.0" encoding="utf-8" ?>
+<nlog xmlns="http://www.nlog-project.org/schemas/NLog.xsd"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+
+  <targets>
+    <!-- Define a target for the log files -->
+    <target name="logfile" xsi:type="File"
+            fileName="${basedir}/logs/log_${shortdate}_${hour}.log"
+            layout="${longdate} ${logger} ${message}" />
+  </targets>
+
+  <rules>
+    <!-- Log all messages to the log file -->
+    <logger name="*" minlevel="Trace" writeTo="logfile" />
+  </rules>
+</nlog>
