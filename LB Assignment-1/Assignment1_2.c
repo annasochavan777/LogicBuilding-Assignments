aa0d4aa9985f49d7097d18e2a7c8dc407eaad57b
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-23 11:15:44 +0530

    Update Assignment1_2.c

--- LB Assignment-1/Assignment1_2.c
+++ LB Assignment-1/Assignment1_2.c
@@ -1,1 +1,3 @@
-Subject: Urgent: Performance Issues with Jenkins Build Controller Causing Delays
+Certainly! Here's the rephrased version:
+
+"I apologize for the misunderstanding earlier; I was considering other fields. Regarding the date field, one scenario that comes to mind is that if no case requirements are created for a case, then the case requirements date would be null."
