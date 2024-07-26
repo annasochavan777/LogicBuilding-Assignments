cb6507bae4dbe2ba488916d5ef88f5f21a3d393e
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-26 17:06:07 +0530

    Update Assignment1_2.c

--- LB Assignment-1/Assignment1_2.c
+++ LB Assignment-1/Assignment1_2.c
@@ -1,1 +1,11 @@
-Are you forwarding the case requirements to the MSO? I don't see any requests. The Upload Document API is responsible for storing those requirements in the MSO database. If you are calling this API, please provide me with the correlation ID so I can investigate further.
+# Sample string containing a GUID
+$string = "Here is a sample string with a GUID: 123e4567-e89b-12d3-a456-426614174000 and some other text."
+
+# Regular expression pattern for GUID
+$pattern = '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}'
+
+# Extract the GUID from the string
+$guid = [regex]::match($string, $pattern).Value
+
+# Output the extracted GUID
+Write-Output $guid
