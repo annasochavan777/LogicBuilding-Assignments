0a3dd071810d73224d1e6947cbd7ceac2c3e340c
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-07-29 13:04:56 +0530

    Update Assignment1_2.c

--- LB Assignment-1/Assignment1_2.c
+++ LB Assignment-1/Assignment1_2.c
@@ -1,11 +1,1 @@
-# Sample string containing a GUID
-$string = "Here is a sample string with a GUID: 123e4567-e89b-12d3-a456-426614174000 and some other text."
-
-# Regular expression pattern for GUID
-$pattern = '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}'
-
-# Extract the GUID from the string
-$guid = [regex]::match($string, $pattern).Value
-
-# Output the extracted GUID
-Write-Output $guid
+We are seeing this error message in our logs, indicating that we have reached the daily limit for online bookings. Can we increase that limit?
