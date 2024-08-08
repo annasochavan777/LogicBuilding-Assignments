f41e2ddd2cbfe0dea8d79dd36fffc6c12ca1f264
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-08-08 10:20:33 +0530

    Update Assignment1_2.c

--- LB Assignment-1/Assignment1_2.c
+++ LB Assignment-1/Assignment1_2.c
@@ -1,1 +1,8 @@
-“Out of all the reported fields, only 27 appear to be relevant. Three of these have been successfully stored in the case Hive, but the remaining 24 have not. We need to correct the mapping for those fields.”
+filebeat.inputs:
+- type: log
+  enabled: true
+  paths:
+    - /path/to/your/log/*.log
+  fields:
+    log_type: your_log_type
+  fields_under_root: true
