c16e7c48ebe0e2926e7d05505cffdf9ecb84cec0
annasochavan777 <40637191+annasochavan777@users.noreply.github.com>
2024-08-09 18:34:42 +0530

    Update Assignment1_2.c

--- LB Assignment-1/Assignment1_2.c
+++ LB Assignment-1/Assignment1_2.c
@@ -1,60 +1,38 @@
 using System;
 using System.Collections.Generic;
-using System.Reflection;
+using System.Text;
 
-public class ObjectToStringDictionaryConverter
+public class DictionaryToStringConverter
 {
-    public static List<string> ConvertToStringDictionary(object obj)
+    public static string ConvertDictionaryToString(Dictionary<string, object> dictionary)
     {
-        if (obj == null)
-            throw new ArgumentNullException(nameof(obj));
+        if (dictionary == null)
+            throw new ArgumentNullException(nameof(dictionary));
 
-        var stringList = new List<string>();
+        var stringBuilder = new StringBuilder();
 
-        // Get the type of the object
-        Type type = obj.GetType();
-
-        // Iterate over all properties of the object
-        foreach (PropertyInfo property in type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+        foreach (var kvp in dictionary)
         {
-            // Get the property name
-            string propertyName = property.Name;
-
-            // Get the value of the property
-            object propertyValue = property.GetValue(obj, null);
-
-            // Add the formatted string to the list
-            stringList.Add($"key: {propertyName}, value: {propertyValue}");
+            stringBuilder.AppendLine($"key: {kvp.Key}, value: {kvp.Value}");
         }
 
-        return stringList;
+        return stringBuilder.ToString();
     }
-}
-
-public class Example
-{
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public bool IsEmployed { get; set; }
 }
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        var exampleObject = new Example
+        var dictionary = new Dictionary<string, object>
         {
-            Name = "John Doe",
-            Age = 30,
-            IsEmployed = true
+            { "Name", "John Doe" },
+            { "Age", 30 },
+            { "IsEmployed", true }
         };
 
-        var stringDictionary = ObjectToStringDictionaryConverter.ConvertToStringDictionary(exampleObject);
+        string result = DictionaryToStringConverter.ConvertDictionaryToString(dictionary);
 
-        // Print the string representations
-        foreach (var entry in stringDictionary)
-        {
-            Console.WriteLine(entry);
-        }
+        Console.WriteLine(result);
     }
 }
